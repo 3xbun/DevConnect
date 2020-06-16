06c672cac4b074ca2da92be579fbb8fb20b65bd3
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-06-16 18:52:56 +0700

    Create route file

---
+++ routes/api/profile.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/profiles
+// @desc    Test route
+// @access  Public
+router.get('/', (req, res) => res.send("profiles routes"));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ connectDB()
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send("Hello World"));
+// Routes
+app.use('/api/users', require('./routes/api/users.routes'));
+app.use('/api/auth', require('./routes/api/auth.routes'));
+app.use('/api/posts', require('./routes/api/posts.routes'));
+app.use('/api/profile', require('./routes/api/profile.routes'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
