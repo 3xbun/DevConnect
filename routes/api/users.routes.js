b557e6bc9bb3f6a518329689481446e2d3061a74
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-06-16 19:11:39 +0700

    Create validators

--- routes/api/users.routes.js
+++ routes/api/users.routes.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Registering user
 // @access  Public
-router.get('/', (req, res) => res.send("users routes"));
+router.post("/", [
+  check('name', "Name is required").not().isEmpty(),
+  check('email', "Email invalid").isEmail(),
+  check('password', "Password invalid").isLength({min: 6}),
+], (req, res) => {
+  const err = validationResult(req);
+
+  if (!err.isEmpty()) {
+    return res.status(400).json({ errors: err.array() });
+  }
+
+  console.log(req.body);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect MongoDB
 connectDB()
 
+// Middleware
+app.use(express.json({ extended: false }))
+
 const PORT = process.env.PORT || 5000;
 
 // Routes
